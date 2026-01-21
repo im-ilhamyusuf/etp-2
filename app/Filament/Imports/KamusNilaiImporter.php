74438b1ae32e5280a9247f0606839439eac7453c
ilham yusuf <ilham.yusuf@itg.ac.id>
2026-01-21 21:30:24 +0700

    add impor kamus nilai

---
+++ app/Filament/Imports/KamusNilaiImporter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Imports;
+
+use App\Models\KamusNilai;
+use Filament\Actions\Imports\ImportColumn;
+use Filament\Actions\Imports\Importer;
+use Filament\Actions\Imports\Models\Import;
+use Illuminate\Support\Number;
+
+class KamusNilaiImporter extends Importer
+{
+    protected static ?string $model = KamusNilai::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ImportColumn::make('jumlah_benar')
+                ->requiredMapping()
+                ->numeric()
+                ->rules(['required', 'integer']),
+            ImportColumn::make('listening')
+                ->requiredMapping()
+                ->numeric()
+                ->rules(['required', 'integer']),
+            ImportColumn::make('reading')
+                ->requiredMapping()
+                ->numeric()
+                ->rules(['required', 'integer']),
+            ImportColumn::make('structure')
+                ->requiredMapping()
+                ->numeric()
+                ->rules(['required', 'integer']),
+        ];
+    }
+
+    public function resolveRecord(): KamusNilai
+    {
+        return new KamusNilai();
+    }
+
+    public static function getCompletedNotificationBody(Import $import): string
+    {
+        $body = 'Impor kamus nilai selesai. '
+            . Number::format($import->successful_rows)
+            . ' baris berhasil diimpor.';
+
+        if ($failedRowsCount = $import->getFailedRowsCount()) {
+            $body .= ' '
+                . Number::format($failedRowsCount)
+                . ' baris gagal diimpor.';
+        }
+
+        return $body;
+    }
+}
--- app/Filament/Resources/KamusNilais/Tables/KamusNilaisTable.php
+++ app/Filament/Resources/KamusNilais/Tables/KamusNilaisTable.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\KamusNilais\Tables;
 
+use App\Filament\Imports\KamusNilaiImporter;
+use Filament\Actions\BulkAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ImportAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -29,9 +32,16 @@ class KamusNilaisTable
             ->filters([
                 //
             ])
+            ->headerActions([
+                ImportAction::make()
+                    ->importer(KamusNilaiImporter::class)
+            ])
             ->recordActions([
                 EditAction::make(),
                 DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                DeleteBulkAction::make()
             ]);
     }
 }
