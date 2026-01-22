7d3b34f79efdd4b78b67007a3b51e4f32a9cc7f4
ilham yusuf <ilham.yusuf@itg.ac.id>
2026-01-22 10:58:27 +0700

    style(admin): responsive urutan form & foto di PesertaForm

--- app/Filament/Resources/Pesertas/Schemas/PesertaForm.php
+++ app/Filament/Resources/Pesertas/Schemas/PesertaForm.php
@@ -8,6 +8,7 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Components\Group;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
@@ -18,84 +19,117 @@ class PesertaForm
     {
         return $schema
             ->components([
-                Section::make("Formulir Biodata Peserta")
-                    ->icon(Heroicon::OutlinedDocumentText)
+                Group::make()
                     ->columnSpan(2)
-                    ->columns(2)
                     ->schema([
-                        Select::make('user_id')
-                            ->label("Akun user")
-                            ->options(User::user()->pluck('name', 'id'))
-                            ->searchable(),
-                        TextInput::make('no_peserta')
-                            ->disabled()
-                            ->dehydrated(false)
-                            ->placeholder('Otomatis dibuat sistem'),
-                        Select::make('jenis_kelamin')
-                            ->options(['L' => 'Laki-laki', 'P' => 'Perempuan'])
-                            ->required()
-                            ->searchable(),
-                        TextInput::make('nik')
-                            ->label("NIK")
-                            ->required()
-                            ->unique(),
-                        TextInput::make('tempat_lahir')
-                            ->required(),
-                        DatePicker::make('tanggal_lahir')
-                            ->required(),
-                        Select::make('agama')
-                            ->options([
-                                'Islam' => 'Islam',
-                                'Kristen' => 'Kristen',
-                                'Katolik' => 'Katolik',
-                                'Hindu' => 'Hindu',
-                                'Buddha' => 'Buddha',
-                                'Konghucu' => 'Konghucu',
-                            ])
-                            ->required()
-                            ->searchable(),
-                        Select::make('pendidikan_terakhir')
-                            ->options([
-                                'Tidak Tamat SD' => 'Tidak tamat SD',
-                                'SD' => 'SD',
-                                'SMP' => 'SMP',
-                                'SMA/SMK' => 'SMA/SMK',
-                                'D3' => 'D3',
-                                'S1' => 'S1',
-                                'S2' => 'S2',
-                                'S3' => 'S3',
-                            ])
-                            ->required()
-                            ->searchable(),
-                        TextInput::make('tahun_lulus'),
-                        TextInput::make('pekerjaan'),
-                        TextInput::make('instansi'),
-                        TextInput::make('nim')
-                            ->label("NIM"),
-                        TextInput::make('nidn')
-                            ->label("NIDN"),
-                        Select::make('kewarganegaraan')
-                            ->options(['WNI' => 'WNI', 'WNA' => 'WNA'])
-                            ->required()
-                            ->searchable(),
-                        TextInput::make('bahasa'),
-                        TextInput::make('no_hp')
-                            ->required(),
-                        Textarea::make('alamat')
-                            ->required()
-                            ->columnSpanFull(),
+                        Section::make('Akun')
+                            ->icon(Heroicon::OutlinedUserCircle)
+                            ->collapsed(false)
+                            ->columns(2)
+                            ->schema([
+                                Select::make('user_id')
+                                    ->label("Akun User")
+                                    ->options(User::user()->pluck('name', 'id'))
+                                    ->required(),
+                                TextInput::make('no_peserta')
+                                    ->disabled()
+                                    ->dehydrated(false)
+                                    ->placeholder("No peserta otomatis dibuatkan oleh sistem"),
+                            ]),
+
+                        Section::make('Biodata')
+                            ->icon(Heroicon::OutlinedIdentification)
+                            ->collapsed(false)
+                            ->columns(2)
+                            ->schema([
+                                TextInput::make('nik')
+                                    ->required(),
+                                Select::make('jenis_kelamin')
+                                    ->options(['L' => 'Laki-laki', 'P' => 'Perempuan'])
+                                    ->required()
+                                    ->searchable(),
+                                TextInput::make('tempat_lahir')
+                                    ->required(),
+                                DatePicker::make('tanggal_lahir')
+                                    ->required(),
+                                Select::make('agama')
+                                    ->options([
+                                        'Islam' => 'Islam',
+                                        'Kristen' => 'Kristen',
+                                        'Katolik' => 'Katolik',
+                                        'Hindu' => 'Hindu',
+                                        'Buddha' => 'Buddha',
+                                        'Konghucu' => 'Konghucu',
+                                    ])
+                                    ->required()
+                                    ->searchable(),
+                                Select::make('kewarganegaraan')
+                                    ->options(['WNI' => 'WNI', 'WNA' => 'WNA'])
+                                    ->required()
+                                    ->searchable(),
+                                TextInput::make('bahasa'),
+                                TextInput::make('no_hp')
+                                    ->required(),
+                                Textarea::make('alamat')
+                                    ->required()
+                                    ->columnSpanFull(),
+                            ]),
+
+                        Section::make('Pendidikan')
+                            ->icon(Heroicon::OutlinedAcademicCap)
+                            ->collapsed(false)
+                            ->columns(2)
+                            ->schema([
+                                Select::make('pendidikan_terakhir')
+                                    ->options([
+                                        'Tidak Tamat SD' => 'Tidak tamat SD',
+                                        'SD' => 'SD',
+                                        'SMP' => 'SMP',
+                                        'SMA/SMK' => 'SMA/SMK',
+                                        'D3' => 'D3',
+                                        'S1' => 'S1',
+                                        'S2' => 'S2',
+                                        'S3' => 'S3',
+                                    ])
+                                    ->required()
+                                    ->searchable(),
+                                TextInput::make('tahun_lulus'),
+                            ]),
+
+                        Section::make('Pekerjaan')
+                            ->icon(Heroicon::OutlinedBriefcase)
+                            ->collapsed(false)
+                            ->columns(2)
+                            ->schema([
+                                TextInput::make('pekerjaan'),
+                                TextInput::make('instansi'),
+                                TextInput::make('nim')
+                                    ->label("NIM")
+                                    ->belowContent(components: "Isi jika pekerjaan Mahasiswa"),
+                                TextInput::make('nidn')
+                                    ->label("NIDN")
+                                    ->belowContent("Isi jika pekerjaan Dosen"),
+                            ]),
+                    ])
+                    ->columnOrder([
+                        'default' => 2,
+                        'lg' => 1
                     ]),
 
-                Section::make("Pas Foto")
-                    ->icon(Heroicon::OutlinedPhoto)
-                    ->schema([
-                        FileUpload::make('foto')
-                            ->image()
-                            ->imageEditor()
-                            ->imageCropAspectRatio('2:3')
-                            ->disk('public')
-                            ->directory('peserta')
-                    ])
+                    Section::make("Pas Foto")
+                        ->icon(Heroicon::OutlinedPhoto)
+                        ->schema([
+                            FileUpload::make('foto')
+                                ->image()
+                                ->imageEditor()
+                                ->imageCropAspectRatio('2:3')
+                                ->disk('public')
+                                ->directory('peserta')
+                        ])
+                        ->columnOrder([
+                            'default' => 1,
+                            'lg' => 2
+                        ]),
             ])
             ->columns(3);
     }
