a29089a1792de32dfbb82f6b42d3beab95020e01
ilham yusuf <ilham.yusuf@itg.ac.id>
2026-02-09 11:16:26 +0700

    style(app): perbaikan page title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
