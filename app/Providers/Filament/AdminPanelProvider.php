cf640b1bfa164ece54d751df5eb480474a10d26b
ilham yusuf <ilham.yusuf@itg.ac.id>
2026-06-20 08:42:28 +0700

    update sidebar collapse

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -49,6 +49,7 @@ class AdminPanelProvider extends PanelProvider
             )
             ->resourceCreatePageRedirect('index')
             ->readOnlyRelationManagersOnResourceViewPagesByDefault(false)
+            ->sidebarCollapsibleOnDesktop()
             ->navigationGroups([
                 'Master',
                 'Short Course',
